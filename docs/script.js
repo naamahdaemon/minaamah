95dfb42496996cba1e6effddce6fecd748e4e05d
Naamah <tlefeuvre@gmail.com>
2023-08-04 15:00:44 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,7 @@
 const queryParams = new URLSearchParams(window.location.search);
 var page = queryParams.get('page'); 
 if (page === null) {
-		  page = "index.md";
+   page = "index.md";
+else
+   page = page + ".md";
 }
