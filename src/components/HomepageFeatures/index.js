61f164f1463fe49b46bec922d29fd1dbdf3fa37e
Naamah <tlefeuvre@gmail.com>
2024-01-11 00:10:24 +0100

    Start customizing doc website

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,6 +4,17 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
+    title: 'Welcome to my Mina Protocol Validator Web  Site',
+    Svg: require('@site/static/img/mina_logo.svg').default,
+    description: (
+      <>
+        This web site is about Mina Protocol.<br/>
+		<br/>
+		(In Construction)
+      </>
+    ),
+  }
+/*  {
     title: 'Easy to Use',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
@@ -32,12 +43,12 @@ const FeatureList = [
         be extended while reusing the same header and footer.
       </>
     ),
-  },
+  },*/
 ];
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col text-center')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -53,7 +64,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className="row justify-content-center">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
