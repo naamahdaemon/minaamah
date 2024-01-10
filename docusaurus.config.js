413fc045c2a3bf5854c48f6c3b600a082e7b20b3
Naamah <tlefeuvre@gmail.com>
2024-01-10 17:00:03 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   trailingSlash: false,
 
   // Set the production url of your site here
-  url: 'https://mina.naamahdaemon.eu/minaamah',
+  url: 'https://mina.naamahdaemon.eu/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/minnamah/minaamah/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
